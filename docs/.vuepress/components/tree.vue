147dd808cab8f814a07cd3601c3ca70746d94b8b
GavinLam164 <m13590491852@163.com>
2021-06-17 14:43:00 +0800

    sort intro

---
+++ docs/.vuepress/components/tree.vue
@@ -0,0 +1,27 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+export default {
+	props: {
+		treeRoot: Object
+	},
+	data() {
+		return {
+			positionMap: new Map(),
+			canvas: null
+		}
+	},
+	mounted() {
+		this.canvas = this.$refs.canvas
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style>
+
+</style>
